9381cbb0caa61b3bc0c264c4877ca8a4cb8f095d
JaronKing <jaronjking@gmail.com>
2023-11-02 01:30:45 -0700

    Home page now a route with a pages and routes folder created.  Import route to be used and Home component.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-
+import {
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import { rootPath } from "./routes/Routes"
 function App() {
     return (
-    <>
-        <div className="bg-slate-800 min-w-full min-h-screen relative">
-            <header className="w-full text-2xl fixed">
-                <div className="flex flex-row bg-sky-950 m-auto max-w-7xl p-5 shadow-lg">
-                    <div className="m-auto text-center text-7xl font-bold text-neutral-950">
-                        Halloween Meme Factory
-                    </div>
-                </div>
-            </header>
-            <div className="max-w-7xl m-auto grid grid-cols-4 gap-5 pb-24 pt-36">
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+        <Routes>
+            <Route exact path={rootPath} element={<Home />} />
 
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md meme-loading"></div>
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md meme-loading"></div>
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md meme-loading"></div>
-                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md meme-loading"></div>
-            </div>
-        </div>
-
-    </>
+        </Routes>
     );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+
+function Home() {
+    return (
+    <>
+        <div className="bg-slate-800 min-w-full min-h-screen relative">
+            <header className="w-full text-2xl fixed">
+                <div className="flex flex-row bg-sky-950 m-auto max-w-7xl p-5 shadow-lg">
+                    <div className="m-auto text-center text-7xl font-bold text-neutral-950">
+                        Halloween Meme Factory
+                    </div>
+                </div>
+            </header>
+            <div className="max-w-7xl m-auto grid grid-cols-4 gap-5 pb-24 pt-36">
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md meme-loading"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md meme-loading"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md meme-loading"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md meme-loading"></div>
+            </div>
+        </div>
+
+    </>
+    );
+}
+
+export default Home;
