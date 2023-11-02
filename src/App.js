5a338e9a7fdf7b5b53e4ed5080c8b51651f6693b
JaronKing <jaronjking@gmail.com>
2023-11-02 01:24:25 -0700

    First commit and first page.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+    <>
+        <div className="bg-slate-800 min-w-full min-h-screen relative">
+            <header className="w-full text-2xl fixed">
+                <div className="flex flex-row bg-sky-950 m-auto max-w-7xl p-5 shadow-lg">
+                    <div className="m-auto text-center text-7xl font-bold text-neutral-950">
+                        Halloween Meme Factory
+                    </div>
+                </div>
+            </header>
+            <div className="max-w-7xl m-auto grid grid-cols-4 gap-5 pb-24 pt-36">
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md"></div>
+
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md meme-loading"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md meme-loading"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md meme-loading"></div>
+                <div className="bg-neutral-900 w-full h-60 shadow-lg rounded-md meme-loading"></div>
+            </div>
+        </div>
+
+    </>
+    );
 }
 
 export default App;
